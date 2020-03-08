7596a881e5f016666a373e701b222928b99b063a
Arturo Herrán <50429024+arturoherran@users.noreply.github.com>
2020-03-08 14:54:58 -0600

    including keydown for clearing and changing word

--- js/script.js
+++ js/script.js
@@ -1,118 +1,70 @@
-  function drawText() {
+function drawText() {
   
-    let canvas = document.getElementById("ltpcanvas");
-    let context = canvas.getContext("2d");
-    context.font = 'bold 68px Poiret One';
+  let canvas = document.getElementById("ltpcanvas");
+  let context = canvas.getContext("2d");
+  context.font = 'bold 68px Poiret One';
 
 
-    let word = window.prompt("Escribe la palabra, krnal");
+  let word = window.prompt("Escribe la palabra, krnal");
 
-    //user color definition
-    // let colorContorno = window.prompt("Qué color quieres el contorno");
-    // colorContorno = colorContorno.toLowerCase;
-    // let colorRelleno = window.prompt("Y el relleno?");
-    // colorRelleno = colorRelleno.toLowerCase;
+  // // user color definition
+  // let colorContorno = window.prompt("Qué color quieres el contorno (en ingleis)");
+  // colorContorno.toLowerCase;
+  // let colorRelleno = window.prompt("Y el relleno? (en ingleis aussi)");
+  // colorRelleno.toLowerCase;
 
-    let screenWidth = Math.floor(window.innerWidth);
-    let screenHeight = Math.floor(window.innerHeight);
-    let canvasWidth = Math.floor(screenWidth*0.8);
-    let canvasHeight = Math.floor(screenHeight*0.8);
-    let sideSpace = Math.floor((screenWidth-canvasWidth)/2);
-    let upNdownSpace = Math.floor((screenHeight-canvasHeight)/2);
-    let wordWidth = Math.floor(context.measureText(word).width);
-    let wordWidthRound = Math.floor(wordWidth);
-    let initX = sideSpace;
-    let endX = (screenWidth-sideSpace); 
-    let initY = upNdownSpace;
-    let endY = (screenHeight-upNdownSpace);
+  let screenWidth = Math.floor(window.innerWidth);
+  let screenHeight = Math.floor(window.innerHeight);
+  let canvasWidth = Math.floor(screenWidth*0.8);
+  let canvasHeight = Math.floor(screenHeight*0.8);
+  let sideSpace = Math.floor((screenWidth-canvasWidth)/2);
+  let upNdownSpace = Math.floor((screenHeight-canvasHeight)/2);
+  let wordWidth = Math.floor(context.measureText(word).width);
+  let wordWidthRound = Math.floor(wordWidth);
+  let initX = sideSpace;
+  let endX = (screenWidth-sideSpace); 
+  let initY = upNdownSpace;
+  let endY = (screenHeight-upNdownSpace);
 
-    //horizontal control
-    let wordCount = Math.floor(canvasWidth/wordWidthRound);
-    let wordCountTotal = wordCount*wordWidthRound;
-    let canvasBlankSpace = canvasWidth-wordCountTotal;
-    let totalBlankSpaces = wordCount-1;
-    let individualBlankWidth = canvasBlankSpace/totalBlankSpaces;
-    let xStep = wordWidthRound+individualBlankWidth;
+  //horizontal control
+  let wordCount = Math.floor(canvasWidth/wordWidthRound);
+  let wordCountTotal = wordCount*wordWidthRound;
+  let canvasBlankSpace = canvasWidth-wordCountTotal;
+  let totalBlankSpaces = wordCount-1;
+  let individualBlankWidth = canvasBlankSpace/totalBlankSpaces;
+  let xStep = wordWidthRound+individualBlankWidth;
 
-    //vertical control
-    let verticalCount = Math.floor(canvasHeight/30);
+  //vertical control
+  let verticalCount = Math.floor(canvasHeight/30);
 
-    //validating in console
-    console.log(screenWidth + " de ancho & de alto (pantalla) " + screenHeight);
-    console.log(canvasWidth + " de ancho & de alto (canvas) " + canvasHeight);
-    console.log("El espacio que sobra a cada lado es: " + sideSpace);
-    console.log("El espacio que sobra arriba y abajo es: " + upNdownSpace);
-    console.log("el init en X tendría sentido que fuera en: " + initX + " y el end en: " + endX);
-    console.log("el init en Y tendría sentido que fuera en: " + initY + " y el end en: " + endY);
-    console.log("Ancho de la palabra: "+ wordWidthRound +"px");
-    console.log("El espacio total ocupado por las palabras es: " + wordCountTotal);
-    console.log("El espacio total libre en el canvas es: " + canvasBlankSpace);
-    console.log("el ancho de cada espacio es: " + individualBlankWidth);
-    console.log("verticalmente caben: " + verticalCount);
-    
-    if(wordCount>=3){
-      for(let i=0; i<wordCount; i++){
-        initY = upNdownSpace;
-
-        for(let j=0; j<verticalCount;j++) {
-          context.lineWidth = 2;
-          context.strokeStyle = "black";
-          context.strokeText(word,initX,initY);
-          context.fillStyle = "red";    
-          context.fillText(word,initX,initY);
-          initY+=34;  
-        }
-
-        initX+=xStep;
-      }
-    }
-
-    else if(wordCount===2) {
-      totalBlankSpaces = 3;
-      individualBlankWidth = canvasBlankSpace/totalBlankSpaces;
-      initX = sideSpace+individualBlankWidth;
-      xStep = wordWidthRound+individualBlankWidth;
-
-      for(let i=0; i<wordCount; i++){
-        initY = upNdownSpace;
-
-        for(let j=0; j<verticalCount;j++) {
-          context.lineWidth = 2;
-          context.strokeStyle = "black";
-          context.strokeText(word,initX,initY);
-          context.fillStyle = "red";    
-          context.fillText(word,initX,initY);
-          initY+=34;  
-        }
-
-        initX+=xStep;
-      }
-
-    }
-    
-
-      
-
-        // for(let i=0; i<wordCount; i++){
-        //   initY = upNdownSpace;
-
-        //   for(let j=0; j<verticalCount;j++) {
-        //     context.lineWidth = 2;
-        //     context.strokeStyle = "black";
-        //     context.strokeText(word,initX,initY);
-        //     context.fillStyle = "red";    
-        //     context.fillText(word,initX,initY);
-        //     initY+=34;  
-        //   }
-
-        //   initX+=xStep;
-        // }
-                
-  }
+  //validating in console
+  console.log(screenWidth + " de ancho & de alto (pantalla) " + screenHeight);
+  console.log(canvasWidth + " de ancho & de alto (canvas) " + canvasHeight);
+  console.log("El espacio que sobra a cada lado es: " + sideSpace);
+  console.log("El espacio que sobra arriba y abajo es: " + upNdownSpace);
+  console.log("el init en X tendría sentido que fuera en: " + initX + " y el end en: " + endX);
+  console.log("el init en Y tendría sentido que fuera en: " + initY + " y el end en: " + endY);
+  console.log("Ancho de la palabra: "+ wordWidthRound +"px");
+  console.log("El espacio total ocupado por las palabras es: " + wordCountTotal);
+  console.log("El espacio total libre en el canvas es: " + canvasBlankSpace);
+  console.log("el ancho de cada espacio es: " + individualBlankWidth);
+  console.log("verticalmente caben: " + verticalCount);
   
-  drawText();
+  //clear context to redraw
 
-  function print() {
+  //wordCount bigger than 3, this is to controll the blank spaces between words
+
+  context.clearRect(0,0,screenWidth,screenHeight);
+
+  if(wordCount>=3){
+
+    //to keep words legible we have to control the spaces between them, if space is less than 20px we will
+    //decrease the count by one
+    // if(individualBlankWidth<20) {
+    //   wordCount--;
+    // } 
+
+    //block of code to print words on canvas
     for(let i=0; i<wordCount; i++){
       initY = upNdownSpace;
 
@@ -127,4 +79,56 @@
 
       initX+=xStep;
     }
+
   }
+
+  else if(wordCount===2) {
+    totalBlankSpaces = 3;
+    individualBlankWidth = canvasBlankSpace/totalBlankSpaces;
+    initX = sideSpace+individualBlankWidth;
+    xStep = wordWidthRound+individualBlankWidth;
+
+    for(let i=0; i<wordCount; i++){
+      initY = upNdownSpace;
+
+      for(let j=0; j<verticalCount;j++) {
+        context.lineWidth = 2;
+        context.strokeStyle = 'black';
+        context.strokeText(word,initX,initY);
+        context.fillStyle = 'red' ;    
+        context.fillText(word,initX,initY);
+        initY+=34;  
+      }
+
+      initX+=xStep;
+    }
+
+  }
+  
+
+    
+
+      // for(let i=0; i<wordCount; i++){
+      //   initY = upNdownSpace;
+
+      //   for(let j=0; j<verticalCount;j++) {
+      //     context.lineWidth = 2;
+      //     context.strokeStyle = "black";
+      //     context.strokeText(word,initX,initY);
+      //     context.fillStyle = "red";    
+      //     context.fillText(word,initX,initY);
+      //     initY+=34;  
+      //   }
+
+      //   initX+=xStep;
+      // }
+             
+      
+}
+
+
+document.addEventListener('keypress', function (e) {
+  if(13 == e.keyCode) {
+    drawText();
+  }
+})
